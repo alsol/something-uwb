b6984be7d6c8db7f5eab11fb75a1a0d065f5dde8
alsol <xerox.vrn@gmail.com>
2017-01-22 18:54:46 +0300

    Added interpolation for surface

--- waveforms/maximuses_all/PlotMax.m
+++ waveforms/maximuses_all/PlotMax.m
@@ -1,19 +1,34 @@
 listing = dir('D:/waveforms/maximuses_all/*.txt');
 listing = {listing.name};
-scatter = zeros(50,50);
 
+data = [];
 for folder = 1:length(listing)
     file = fullfile('D:/waveforms/maximuses_all/', listing{folder});  
     input = fopen(file,'r');
     formatSpec = '%f %f %f\n';
-    data = fscanf(input, formatSpec,[3 Inf]);
+    data = [data fscanf(input, formatSpec,[3 Inf])];
     fclose(input);
-    
-    for i = 1:length(data(1,:))-1
-        scatter(data(1,i)+1,data(2,i)+1) = data(3,i+1);
-    end;
 end;
 
-surf(scatter);
-xlabel('i');
-ylabel('j');
+x_o = data(1,:);
+y_o = data(2,:);
+z_o = data(3,:);
+
+%% Fit.
+[xData, yData, zData] = prepareSurfaceData( x_o, y_o, z_o );
+
+% Set up fittype and options.
+ft = 'linearinterp';
+
+% Fit model to data.
+[fitresult, gof] = fit( [xData, yData], zData, ft, 'Normalize', 'on' );
+
+% Plot fit with data.
+figure( 'Name', 'untitled fit 1' );
+h = plot( fitresult, [xData, yData], zData );
+legend( h, 'untitled fit 1', 'z_o vs. x_o, y_o', 'Location', 'NorthEast' );
+% Label axes
+xlabel x_o
+ylabel y_o
+zlabel z_o
+grid on
